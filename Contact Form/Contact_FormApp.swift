3130abe273a456b6a3d4103f4311abc96e0f3fd7
Eingest <ak0805@live.de>
2022-05-25 13:20:26 +0200

    Initial Commit

---
+++ Contact Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by Andreas Kiesel on 25.05.22.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
