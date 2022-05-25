b9cc73c991f5258d9676c7a8d39c48b1bcc00405
Eingest <ak0805@live.de>
2022-05-25 14:25:55 +0200

    first changes

--- Contact Form/ContentView.swift
+++ Contact Form/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var emailAdress = ""
+    @State private var message = "Write your message here..."
+    @State private var includeLogs: Bool = false
+    @State private var subject = "Help"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("How can we reach you?")) {
+                    TextField("Email Adress", text: $emailAdress)
+                }
+                Section(header: Text("What can we help you with?")) {
+                    Picker("Subject", selection: $subject) {
+                        ForEach(["Help", "Suggestion", "Bug Report"], id: \.self) { subject in
+                            Text(subject)
+                        }
+                    }
+                }
+                Section(header: Text("Briefly explain what's going on")) {
+                    TextEditor(text: $message)
+                }
+                Section(header: Text("This information will be sent anonymously")) {
+                    Toggle("Include Logs", isOn: $includeLogs)
+                }
+                Button("Submit") {
+                    print("Submit Button Tapped")
+                }
+            }
+        }
     }
 }
 
